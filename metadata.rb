6636cb94b752b2e41c8b29719cfb0cb28b09bec9
Chris Pressland <mail@cpressland.io>
2022-03-14 19:43:49 +0000

    Final set of cert fixes... hopefully!

--- metadata.rb
+++ metadata.rb
@@ -1,4 +1,4 @@
 name 'ultron'
 description 'Installs/Configures ClickHouse'
-version '1.1.3'
+version '1.1.4'
 chef_version '>= 16.0'
--- recipes/default.rb
+++ recipes/default.rb
@@ -12,7 +12,7 @@ service 'clickhouse-server' do
   action :nothing
 end
 
-%w(ca.pem cert.pem key.pem).each do |c|
+%w(cert.pem key.pem).each do |c|
   template "/etc/clickhouse-server/#{c}" do
     source "certificates/#{c}"
     owner 'clickhouse'
